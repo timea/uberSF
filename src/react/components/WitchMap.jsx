cb4f1c714c78f2e81ed3aa6b3687266fdb29eede
Timea Kiss <k.timea.k@gmail.com>
2016-10-30 19:38:44 +0100

    Witch is flying on the map
    Closes #3.

--- src/react/components/WitchMap.jsx
+++ src/react/components/WitchMap.jsx
@@ -5,7 +5,7 @@ const SF_POSITION = {
   lng: -122.438
 };
 
-const PUMPKIN = "/images/jack-o-lantern-icon.png";
+const PUMPKIN = "/images/pumpkin-evil-icon-glow.png";
 
 class WitchMap extends React.Component {
   constructor() {
@@ -17,7 +17,9 @@ class WitchMap extends React.Component {
     window.initMap = function() {
       $that.map = new window.google.maps.Map($that.refs.map, {
         center: SF_POSITION,
-        zoom: 16
+        zoom: 34,
+        mapTypeId: 'satellite',
+        zoomControl: false
       });
     }
   }
@@ -64,9 +66,71 @@ class WitchMap extends React.Component {
       }
   }
 
+  componentDidMount() {
+    $('.witch-container>div').focus();
+    var direction = 1; // 1 - top, 2 - right, 3 - bottom, 4 - left
+    function getRotationDegrees(obj) {
+      var matrix = obj.css("-webkit-transform") ||
+      obj.css("-moz-transform")    ||
+      obj.css("-ms-transform")     ||
+      obj.css("-o-transform")      ||
+      obj.css("transform");
+      if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+      } else { var angle = 0; }
+      return (angle < 0) ? angle +=360 : angle;
+    }
+
+    function rotateWitch(angle) {
+        var $elem = $('#witch');
+        var $start = getRotationDegrees($elem);
+
+        $({deg: $start}).animate({deg: angle}, {
+            duration: 500,
+            step: function(now) {
+                $elem.css({
+                    transform: 'rotate(' + now + 'deg)'
+                });
+            }
+        });
+    }
+
+    $('body').on('keydown', function(e) {
+      if (e.keyCode == 39) {
+          if (direction != 2) {
+            rotateWitch(90);
+            direction = 2;
+          }
+        }
+    });
+    $('body').on('keyup', function(e) {
+      if (e.keyCode == 39) {
+        rotateWitch(0);
+        direction = 1;
+      }
+    });
+
+    $('body').on('keydown', function(e) {
+      if (e.keyCode == 37) {
+          if (direction != 4) {
+            rotateWitch(-90);
+            direction = 4;
+          }
+        }
+    });
+    $('body').on('keyup', function(e) {
+      if (e.keyCode == 37) {
+        rotateWitch(360);
+        direction = 1;
+      }
+    });
+  }
+
   render() {
     const mapStyle = {
-      width: '80vw',
       height: '100vh',
       border: '1px solid black'
     };
@@ -74,7 +138,12 @@ class WitchMap extends React.Component {
     return (
       <div>
         <button onClick={this.addMarkers}>Add markers</button>
-        <div ref="map" style={mapStyle}>I should be a map!</div>
+
+
+        <div className="witch-container">
+          <div ref="map" style={mapStyle}>I should be a map!</div>
+          <img id="witch" src="/images/witch-color-s.png" />
+        </div>
       </div>
     );
   }
